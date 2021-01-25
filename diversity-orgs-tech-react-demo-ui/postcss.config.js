520638022d9c937452304e67e390f32ea6e73245
Jay Miller <kjaymiller@gmail.com>
2021-01-25 12:18:47 -0800

    remove old index.css

--- diversity-orgs-tech-react-demo-ui/postcss.config.js
+++ diversity-orgs-tech-react-demo-ui/postcss.config.js
@@ -1,5 +1,13 @@
 const tailwindcss = require('tailwindcss')
 module.exports = {
+    purge: {
+        enabled: true,
+        mode: 'all',
+        content: [
+        './src/**/*/*.jsx',
+        './src/**/*/*.js'
+    ]
+    },
     plugins: [
         tailwindcss('./tailwind.js'),
         require('autoprefixer')
