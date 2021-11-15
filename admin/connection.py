2ee2c54a1758fa309d8a32f1155b1c46d31cd60b
Jay Miller <kjaymiller@gmail.com>
2021-11-15 04:50:51 +0000

    add connection for ES

--- admin/connection.py
+++ admin/connection.py
@@ -1,10 +1,19 @@
 import os
 
 from elastic_enterprise_search import AppSearch
+from elasticsearch import Elasticsearch
 
 app_search = AppSearch(
-    os.environ.get("CLOUD_URI"),
+    os.environ.get("ES_ENT_CLOUD_URI"),
     http_auth=os.environ.get("PRIVATE_AUTH"),
 )
 
 engine_name = os.environ.get("ENGINE_NAME")
+
+elastic_search = Elasticsearch(
+    cloud_id = os.environ.get("ES_CLOUD_ID"),
+    http_auth=[
+        os.environ.get('ES_USER'),
+        os.environ.get('ES_PWD'),
+    ]
+)
