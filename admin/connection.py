2f7890435967df0246b91461984235a455614359
Jay Miller <kjaymiller@gmail.com>
2022-03-01 15:27:19 -0800

    remove ent-search from connections

--- admin/connection.py
+++ admin/connection.py
@@ -1,14 +1,6 @@
 import os
 
-from elastic_enterprise_search import AppSearch
 from elasticsearch import Elasticsearch
-
-app_search = AppSearch(
-    os.environ.get("ES_ENT_CLOUD_URI"),
-    http_auth=os.environ.get("PRIVATE_AUTH"),
-)
-
-engine_name = os.environ.get("ENGINE_NAME")
 
 elastic_search = Elasticsearch(
     cloud_id = os.environ.get("ES_CLOUD_ID"),
